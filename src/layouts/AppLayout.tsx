7409daefdd3cd8e8c9bc2c2a4a1ea1524d6d7e5a
wily ramos <wily.ramos1@unmsm.edu.pe>
2025-02-08 16:25:47 -0500

    fix deploy

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -24,6 +24,7 @@ export default function AppLayout() {
                 <div className="flex justify-center items-center h-screen">
                     <p>Cargando...</p>
                 </div>
+                
             </>
         )
         
